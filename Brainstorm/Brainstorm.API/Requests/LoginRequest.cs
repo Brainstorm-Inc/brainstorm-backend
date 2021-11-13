103d4053816333f00955cf559b568c3aacbfea10
Alex Petrean <alex.petrean@outlook.com>
2021-11-13 10:55:39 +0200

    feat: add LoginRequest class

---
+++ Brainstorm/Brainstorm.API/Requests/LoginRequest.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace Brainstorm.API.Requests
+{
+    public class LoginRequest
+    {
+        public string Email { get; init; }
+        public string Password { get; init; }
+    }
+}

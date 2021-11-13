6e7346ba278a70636fc79cd5ae4a981010ebc934
Alex Petrean <alex.petrean@outlook.com>
2021-11-13 10:55:39 +0200

    feat: add LoginRequest class

--- Brainstorm/Brainstorm.API/Requests/LoginRequest.cs
+++ Brainstorm/Brainstorm.API/Requests/LoginRequest.cs
@@ -1,5 +1,4 @@
-﻿using System.Data;
-using FluentValidation;
+﻿using FluentValidation;
 
 namespace Brainstorm.API.Requests
 {
@@ -7,16 +6,5 @@ namespace Brainstorm.API.Requests
     {
         public string Email { get; init; }
         public string Password { get; init; }
-    }
-
-    public class LoginRequestValidator : AbstractValidator<LoginRequest>
-    {
-        public LoginRequestValidator()
-        {
-            RuleFor(r => r.Email)
-                .EmailAddress().WithMessage("Wrong email format.");
-            RuleFor(r => r.Password)
-                .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
-        }
     }
 }

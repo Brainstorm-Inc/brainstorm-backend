2583405b14586348eeed84f4fcf1e68f11959fb9
Catalin Gama <catalin.gama@ngi.systems>
2021-11-15 18:40:53 +0200

    fix: fixed user model

---
+++ Brainstorm/Brainstorm.Migrations/Migrations/20211115163636_AddedUserModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Booking.Migrations.Migrations
+{
+    public partial class AddedUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppVersions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MajorVersion = table.Column<int>(type: "integer", nullable: false),
+                    MinorVersion = table.Column<int>(type: "integer", nullable: false, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppVersions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false, defaultValue: "FirstName"),
+                    LastName = table.Column<string>(type: "text", nullable: false, defaultValue: "LastName"),
+                    Email = table.Column<string>(type: "text", nullable: false, defaultValue: "first-last@mail.com"),
+                    ProfilePicture = table.Column<string>(type: "text", nullable: true, defaultValue: "https://robohash.org/first-last")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AppVersions",
+                columns: new[] { "Id", "MajorVersion", "MinorVersion" },
+                values: new object[] { new Guid("73f3221f-becb-4561-9e9b-35514c981c77"), 0, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "FirstName", "LastName", "ProfilePicture" },
+                values: new object[] { new Guid("78c14057-e37a-42d3-9969-8064a3ca7f03"), "Fname-Lname@mail.com", "Fname", "Lname", "https://robohash.org/Fname-Lname.png" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppVersions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}

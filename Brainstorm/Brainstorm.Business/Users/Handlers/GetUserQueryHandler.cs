f3fc6e1af44880fbf736cf52c7110436afe6b5d6
Catalin Gama <catalin.gama@ngi.systems>
2021-11-15 18:48:32 +0200

    tryied again

--- Brainstorm/Brainstorm.Business/Users/Handlers/GetUserQueryHandler.cs
+++ Brainstorm/Brainstorm.Business/Users/Handlers/GetUserQueryHandler.cs
@@ -1,7 +1,38 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Brainstorm.Business.AppVersions;
+using Brainstorm.Business.AppVersions.Queries;
+using Brainstorm.Business.AppVersions.Responses;
+using Brainstorm.Business.Users.Queries;
+using Brainstorm.Business.Users.Responses;
+using Brainstorm.Entities;
+using MediatR;
+
 namespace Brainstorm.Business.Users.Handlers
 {
-    public class GetUserQueryHandler
+    public class GetUserQueryHandler : IRequestHandler<GetUserQuery, UserCode>
     {
-        
+        private readonly BrainstormContext _ctx;
+
+        public GetUserQueryHandler(BrainstormContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public Task<UserCode> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            if (!_ctx.Users.Any())
+            {
+                throw new Exception("No user found.");
+            }
+
+            var code = _ctx.Users.OrderByDescending(v => v.Id)
+                .FirstOrDefault()
+                .ToUserCode();
+
+            return Task.FromResult(code);
+        }
     }
 }
--- Brainstorm/Brainstorm.Data/BrainstormContext.cs
+++ Brainstorm/Brainstorm.Data/BrainstormContext.cs
@@ -1,4 +1,5 @@
 using Brainstorm.Entities.AppVersion;
+using Brainstorm.Entities.User;
 using Microsoft.EntityFrameworkCore;
 
 namespace Brainstorm.Entities {
@@ -7,9 +8,11 @@ namespace Brainstorm.Entities {
         public BrainstormContext(DbContextOptions opts) : base(opts) { }
 
         public DbSet<AppVersion.AppVersion> AppVersions { get; init; }
+        public DbSet<User.User> Users { get; init; }
 
         protected override void OnModelCreating(ModelBuilder builder) {
             builder.ApplyConfiguration(new AppVersionConfig());
+            builder.ApplyConfiguration(new UserConfig());
         }
     }
 }

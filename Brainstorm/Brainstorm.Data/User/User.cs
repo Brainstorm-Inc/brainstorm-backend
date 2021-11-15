6b837c9cf0001127caea31b2ed5b31ba87693cc4
Catalin Gama <catalin.gama@ngi.systems>
2021-11-15 18:48:32 +0200

    tryied again

--- Brainstorm/Brainstorm.Data/User/User.cs
+++ Brainstorm/Brainstorm.Data/User/User.cs
@@ -1,7 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks.Dataflow;
+
 namespace Brainstorm.Entities.User
 {
-    public class User
+    public class User : BaseEntity
     {
-        
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Email {get; }
+        public string ProfilePicture {get; }
+
+         
+        public User(string firstName, string lastName, string email)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            ProfilePicture = $"https://robohash.org/{FirstName}-{LastName}.png";
+        }
     }
 }

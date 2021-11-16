80459ebf99bef9ef480fab6db41a623c996967f6
Alex Petrean <alex.petrean@outlook.com>
2021-11-16 09:43:13 +0200

    added AuthController with login endpoint

---
+++ Brainstorm/Brainstorm.API/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Brainstorm.API.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Brainstorm.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginRequest request)
+        {
+            var res = await _mediator.Send(request.ToQuery());
+
+            return Ok(res);
+        }
+    }
+}

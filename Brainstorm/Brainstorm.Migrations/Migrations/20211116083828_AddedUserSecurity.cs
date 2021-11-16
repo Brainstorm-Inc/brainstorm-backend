aba7788f72d6637e7b289d48c6ccfc31bd84ae16
Alex Petrean <alex.petrean@outlook.com>
2021-11-16 10:40:23 +0200

    added user sec

---
+++ Brainstorm/Brainstorm.Migrations/Migrations/20211116083828_AddedUserSecurity.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Booking.Migrations.Migrations
+{
+    public partial class AddedUserSecurity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AppVersions",
+                keyColumn: "Id",
+                keyValue: new Guid("73f3221f-becb-4561-9e9b-35514c981c77"));
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("78c14057-e37a-42d3-9969-8064a3ca7f03"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Salt",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "AppVersions",
+                columns: new[] { "Id", "MajorVersion", "MinorVersion" },
+                values: new object[] { new Guid("ef42a592-4e5b-4808-80ab-3c042b7594ec"), 0, 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DeleteData(
+                table: "AppVersions",
+                keyColumn: "Id",
+                keyValue: new Guid("ef42a592-4e5b-4808-80ab-3c042b7594ec"));
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Users");
+
+            migrationBuilder.InsertData(
+                table: "AppVersions",
+                columns: new[] { "Id", "MajorVersion", "MinorVersion" },
+                values: new object[] { new Guid("73f3221f-becb-4561-9e9b-35514c981c77"), 0, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "FirstName", "LastName", "ProfilePicture" },
+                values: new object[] { new Guid("78c14057-e37a-42d3-9969-8064a3ca7f03"), "Fname-Lname@mail.com", "Fname", "Lname", "https://robohash.org/Fname-Lname.png" });
+        }
+    }
+}

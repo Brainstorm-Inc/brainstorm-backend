cf7e3566a72d80a4310223d774ce76c6de253d9f
Alex Petrean <alex.petrean@outlook.com>
2021-11-16 12:41:28 +0200

    finished signup

--- Brainstorm/Brainstorm.API/Requests/SignupRequestExtentions.cs
+++ Brainstorm/Brainstorm.API/Requests/SignupRequestExtentions.cs
@@ -1,21 +1,21 @@
-﻿using Brainstorm.Business.Auth.Commands;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Brainstorm.Business.Auth.Commands;
 
 namespace Brainstorm.API.Requests
 {
     public static class SignupRequestExtentions
     {
-
         public static SignupCommand ToCommand(this SignupRequest request)
         {
-            return new SignupCommand {
-                Email = request.Email,
-                Password = request.Password,
+            return new SignupCommand
+            {
                 FirstName = request.FirstName,
                 LastName = request.LastName,
+                Email = request.Email,
+                Password = request.Password
             };
         }
     }
--- Brainstorm/Brainstorm.Business/Auth/Responses/SignupResponse.cs
+++ Brainstorm/Brainstorm.Business/Auth/Responses/SignupResponse.cs
@@ -8,5 +8,11 @@ namespace Brainstorm.Business.Auth.Responses
 {
     class SignupResponse
     {
+        public string Token { get; init; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfilePicture { get; set; }
+        public Guid Id { get; set; }
     }
 }

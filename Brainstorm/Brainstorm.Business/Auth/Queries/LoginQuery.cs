dce0f191c5b67671490493f7e6d38b091201ff39
Alex Petrean <alex.petrean@outlook.com>
2021-11-16 09:42:11 +0200

    added LoginQuery

---
+++ Brainstorm/Brainstorm.Business/Auth/Queries/LoginQuery.cs
@@ -0,0 +1,11 @@
+using Brainstorm.Business.Auth.Responses;
+using MediatR;
+
+namespace Brainstorm.Business.Auth.Queries
+{
+    public class LoginQuery : IRequest<LoginResponse>
+    {
+        public string Email { get; init; }
+        public string Password { get; init; }
+    }
+}

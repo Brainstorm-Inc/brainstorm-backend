1629e957b3cb10eef3ab67643b9fc42df6dc0566
Alex Petrean <alex.petrean@outlook.com>
2021-11-16 09:41:43 +0200

    added LoginRequestExtensions

---
+++ Brainstorm/Brainstorm.API/Requests/LoginRequestExtensions.cs
@@ -0,0 +1,16 @@
+using Brainstorm.Business.Auth.Queries;
+
+namespace Brainstorm.API.Requests
+{
+    public static class LoginRequestExtensions
+    {
+        public static LoginQuery ToQuery(this LoginRequest request)
+        {
+            return new LoginQuery
+            {
+                Email = request.Email,
+                Password = request.Password
+            };
+        }
+    }
+}

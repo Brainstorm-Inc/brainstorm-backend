5d2f281129ecd18aeec235f0f4c51259cb8c6f3d
Alex Petrean <alex.petrean@outlook.com>
2021-11-16 10:38:47 +0200

    added user security

--- Brainstorm/Brainstorm.Data/User/UserConfig.cs
+++ Brainstorm/Brainstorm.Data/User/UserConfig.cs
@@ -8,23 +8,32 @@ namespace Brainstorm.Entities.User
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
-            builder.Property(u => u.Id).HasColumnType($"uuid");
-            builder.Property(u => u.FirstName).IsRequired()
-                                                  .HasDefaultValue("FirstName");
-            builder.Property(u => u.LastName).IsRequired()
-                                                 .HasDefaultValue("LastName");
-            builder.Property(u => u.Email).IsRequired()
-                                              .HasDefaultValue("first-last@mail.com");
+            builder.HasIndex(u => u.Email)
+                .IsUnique();
 
+            builder.Property(u => u.Id)
+                .HasColumnType($"uuid");
 
+            builder.Property(u => u.FirstName)
+                .IsRequired()
+                .HasDefaultValue("FirstName");
 
-            builder.Property(u => u.ProfilePicture).HasDefaultValue("https://robohash.org/first-last");
-            builder.HasData(new User
-            {
-                FirstName ="Fname",
-                LastName ="Lname", 
-                Email="Fname-Lname@mail.com"
-            });
+            builder.Property(u => u.LastName)
+                .IsRequired()
+                .HasDefaultValue("LastName");
+
+            builder.Property(u => u.Email)
+                .IsRequired()
+                .HasDefaultValue("first-last@mail.com");
+
+            builder.Property(s => s.Password)
+                .IsRequired();
+
+            builder.Property(s => s.Salt)
+                .IsRequired();
+
+            builder.Property(u => u.ProfilePicture)
+                .HasDefaultValue("https://robohash.org/first-last");
         }
     }
 }

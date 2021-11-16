7f3c73ddd1a2bcfdfc79cad5eef18664b1af3fbf
Alex Petrean <alex.petrean@outlook.com>
2021-11-16 11:50:40 +0200

    finished login

--- Brainstorm/Brainstorm.API/Controllers/AuthController.cs
+++ Brainstorm/Brainstorm.API/Controllers/AuthController.cs
@@ -1,15 +1,29 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Brainstorm.API.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Brainstorm.API.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginRequest request)
+        {
+            var res = await _mediator.Send(request.ToQuery());
+
+            return Ok(res);
+        }
     }
 }

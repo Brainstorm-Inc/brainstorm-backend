f238c5ddd538b81c5f0d49c7a922c9d9a2131743
Alex Petrean <alex.petrean@outlook.com>
2021-11-16 10:38:47 +0200

    added user security

--- Brainstorm/Brainstorm.Data/User/User.cs
+++ Brainstorm/Brainstorm.Data/User/User.cs
@@ -6,9 +6,11 @@ namespace Brainstorm.Entities.User
 {
     public class User : BaseEntity
     {
-        public string FirstName { get; init;}
+        public string FirstName { get; init; }
         public string LastName { get; init; }
-        public string Email {get; init; }
-        public string ProfilePicture {get; init; }
+        public string Email { get; init; }
+        public string Password { get; init; }
+        public string Salt { get; init; }
+        public string ProfilePicture { get; init; }
     }
 }

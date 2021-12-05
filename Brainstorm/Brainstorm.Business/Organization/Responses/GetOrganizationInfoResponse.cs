efb6e3532414be6bfdb7f01fcfe5327cb2d03027
Catalin Gama <catalin.gama@ngi.systems>
2021-12-05 18:57:55 +0200

    Create GetOrganizationInfoResponse.cs

---
+++ Brainstorm/Brainstorm.Business/Organization/Responses/GetOrganizationInfoResponse.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Brainstorm.Entities.User;
+
+namespace Brainstorm.Business.Organization.Responses
+{
+    public class GetOrganizationInfoResponse
+    {
+        public Guid Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public List<User> Users { get; set; }
+        
+        public string Logo { get; set; }
+    }
+}

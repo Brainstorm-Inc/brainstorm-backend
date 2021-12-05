c3397647cdaf10e76d29606a8b2b8312c15d08e0
Catalin Gama <catalin.gama@ngi.systems>
2021-12-05 19:08:50 +0200

    Create GetOrganizationInfoHandler.cs

---
+++ Brainstorm/Brainstorm.Business/Organization/Handlers/GetOrganizationInfoHandler.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Brainstorm.Business.Auth.Responses;
+using Brainstorm.Business.Organization.Queries;
+using Brainstorm.Business.Organization.Responses;
+using Brainstorm.Business.Utils;
+using Brainstorm.Entities;
+using Brainstorm.Entities.User;
+using MediatR;
+
+namespace Brainstorm.Business.Organization.Handlers
+{
+    public class GetOrganizationInfoHandler : IRequestHandler<GetOrganizationInfoQuery,GetOrganizationInfoResponse>
+    {
+        private readonly BrainstormContext _ctx;
+
+        public GetOrganizationInfoHandler(BrainstormContext ctx)
+        {
+            _ctx = ctx;
+        }
+        
+        public Task<GetOrganizationInfoResponse> Handle(GetOrganizationInfoQuery request, CancellationToken cancellationToken)
+        {
+            var org = _ctx.Organizations
+                .FirstOrDefault(u => request.Id == u.Id);
+
+            if (org is null)
+            {
+                throw new Exception($"Organization with ID {request.Id} does not exist");
+            }
+
+            var res = new GetOrganizationInfoResponse
+            {
+                Id = org.Id,
+                Name = org.Name,
+                Users = org.Users as List<User>,
+                Logo = org.LogoLink
+            };
+            return Task.FromResult(res);
+        }
+    }
+}

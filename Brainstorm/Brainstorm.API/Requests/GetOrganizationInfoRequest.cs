8ff46eefe99ddf23fed391eb95ba1b51fa78c400
Catalin Gama <catalin.gama@ngi.systems>
2021-12-05 19:52:26 +0200

    Create GetOrganizationInfoRequest.cs

---
+++ Brainstorm/Brainstorm.API/Requests/GetOrganizationInfoRequest.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+
+namespace Brainstorm.API.Requests
+{
+    public class GetOrganizationInfoRequest
+    {
+        
+    }
+
+    public class GetOrganizationInfoRequestValidator : AbstractValidator<GetOrganizationInfoRequest>
+    {
+        public GetOrganizationInfoRequestValidator()
+        {
+            
+        }
+    }
+}

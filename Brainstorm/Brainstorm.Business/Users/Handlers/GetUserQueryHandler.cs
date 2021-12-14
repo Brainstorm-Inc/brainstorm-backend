435df8f1d5c31c270476f2524bff4bcd4e0b65d6
Alex Petrean <alex.petrean@outlook.com>
2021-12-14 10:19:57 +0200

    refactored ToUserCode to ToUserDetails

--- Brainstorm/Brainstorm.Business/Users/Handlers/GetUserQueryHandler.cs
+++ Brainstorm/Brainstorm.Business/Users/Handlers/GetUserQueryHandler.cs
@@ -12,7 +12,7 @@ using MediatR;
 
 namespace Brainstorm.Business.Users.Handlers;
 
-public class GetUserQueryHandler : IRequestHandler<GetUserQuery, UserCode>
+public class GetUserQueryHandler : IRequestHandler<GetUserQuery, UserDetails>
 {
     private readonly BrainstormContext _ctx;
 

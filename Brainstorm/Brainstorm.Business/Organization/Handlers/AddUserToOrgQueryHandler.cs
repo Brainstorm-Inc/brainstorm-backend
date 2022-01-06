c2e99c85091e0e08bce0feb82b83c494b897e09c
Bee7go <bianca_badaruta@yahoo.com>
2022-01-06 18:57:10 +0200

    add/remove user from organization

---
+++ Brainstorm/Brainstorm.Business/Organization/Handlers/AddUserToOrgQueryHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Brainstorm.Business.Organization.Queries;
+using Brainstorm.Business.Organization.Responses;
+using Brainstorm.Entities;
+using MediatR;
+
+namespace Brainstorm.Business.AppVersions.Handlers;
+
+public class AddUserToOrgQueryHandler : IRequestHandler<AddUserToOrgQuery, AddUserToOrgResponse>
+{
+    private readonly BrainstormContext _ctx;
+
+    public AddUserToOrgQueryHandler(BrainstormContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    public Task<AddUserToOrgResponse> Handle(AddUserToOrgQuery request, CancellationToken cancellationToken)
+    {
+        var orgId = Guid.Parse(request.OrgId);
+        var org = _ctx.Organizations.FirstOrDefault(o => o.Id == orgId);
+
+        if (org is null)
+        {
+            throw new Exception("Organization doesn't exist.");
+        }
+        
+        var userId = Guid.Parse(request.UserId);
+        var user = _ctx.Users.FirstOrDefault(u => u.Id == userId);
+
+        if (user is null)
+        {
+            throw new Exception("User doesn't exists");
+        }
+
+        if (org.Users.FirstOrDefault(u => u.Id == userId) is not null)
+        {
+            throw new Exception("User is already a member in the organization.");
+        }
+
+        org.Users.Add(user);
+        _ctx.SaveChanges();
+
+        return Task.FromResult(new AddUserToOrgResponse());
+    }
+}
---
+++ Brainstorm/Brainstorm.Business/Organization/Queries/RemoveUserFromOrgQuery.cs
@@ -0,0 +1,10 @@
+﻿using Brainstorm.Business.Organization.Responses;
+using MediatR;
+
+namespace Brainstorm.Business.Organization.Queries;
+
+public class RemoveUserFromOrgQuery : IRequest<RemoveUserFromOrgResponse>
+{
+    public string OrgId { get; init; }
+    public string UserId { get; init; }
+}
